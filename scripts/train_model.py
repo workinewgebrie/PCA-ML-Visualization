6fd9515fe78efa3032176ec5c6aa3a826fc3bac1
Workinew Gebrie <gworkinew@gmail.com>
2026-04-08 08:03:43 +0300

    Remove unnecessary blank line in train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -12,7 +12,6 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.decomposition import PCA
-
 warnings.filterwarnings("ignore")
 
 # -----------------------------
